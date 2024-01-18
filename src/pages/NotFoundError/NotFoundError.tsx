19b5c71a04e697caedb713552db05ee6c5c845e4
Anastasiia Kunstman <anastasiia.kunstman@yandex.ru>
2024-01-18 15:37:43 +0500

    ref

--- src/pages/NotFoundError/NotFoundError.tsx
+++ src/pages/NotFoundError/NotFoundError.tsx
@@ -1,14 +1,15 @@
+import React from 'react';
 import './NotFoundError.css';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-function NotFoundError() {
+const NotFoundError: React.FC = () => {
   return (
     <section className='not-found__error'>
       <h1 className='not-found__title'>404</h1>
       <p className='not-found__desc'>Страница не найдена</p>
-      <Link className='not_found__link' to={-1}>Назад</Link>
+      <Link className='not_found__link' to="/">Назад</Link>
     </section>
-  )
-}
+  );
+};
 
 export default NotFoundError;
